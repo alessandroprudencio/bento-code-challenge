b3b36cb5b21f4c895a5c25e9e85c7074c5185b18
Alessandro Prudencio <alessandroconectado@gmail.com>
2025-03-18 10:03:04 -0400

    docs: set up api documentation using swagger

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,23 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Bento API')
+    .setDescription(
+      'RESTful API for integration with the Bento API, calculation of delivery rate with margin and storage of requests.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    autoTagControllers: true,
+  });
+  SwaggerModule.setup('api/docs', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI });
+
+  setupSwagger(app);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 void bootstrap();
