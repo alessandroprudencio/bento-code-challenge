fa2d50344e7b49d6de1447fbfae4ab146cc36689
Alessandro Prudencio <alessandroconectado@gmail.com>
2025-03-19 18:49:09 -0400

    fix: add bento module to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { RequestsModule } from './requests/requests.module';
 import { FirestoreModule } from './firestore/firestore.module';
 import { ConfigModule } from '@nestjs/config';
+import { BentoModule } from './bento/bento.module';
 import firebaseConfig from './config/firebase.config';
+import bentoConfig from './config/bento.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [firebaseConfig],
+      cache: true,
+      load: [firebaseConfig, bentoConfig],
     }),
     FirestoreModule,
     RequestsModule,
+    BentoModule,
   ],
   controllers: [],
   providers: [],
